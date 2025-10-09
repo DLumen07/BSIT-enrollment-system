25d1c4b79f23ce87586bd997422b8b8bfbec081d
Daren De Lumen <delumendaren77@gmail.com>
2025-10-09 03:09:59 +0000

    the blue header on top of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
-      <header className="w-full h-4 bg-primary" />
       <main className="flex-1 flex flex-col items-center justify-center text-center p-4">
         <div className="flex flex-col items-center space-y-6 border rounded-lg shadow-lg p-8 md:p-12">
           <Image
